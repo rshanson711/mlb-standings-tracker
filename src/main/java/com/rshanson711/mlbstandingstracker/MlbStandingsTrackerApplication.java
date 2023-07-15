191ce57b81823012eab3681b15cb978b2f499cc9
Ray Hanson <45704800+rshanson711@users.noreply.github.com>
2023-07-15 01:47:59 -0500

    Initial commit

---
+++ src/main/java/com/rshanson711/mlbstandingstracker/MlbStandingsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rshanson711.mlbstandingstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbStandingsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbStandingsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rshanson711/mlbstandingstracker/MlbStandingsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rshanson711.mlbstandingstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbStandingsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
