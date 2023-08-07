b477cf398cb61ba878f6677bf771610a2208438c
rshanson711 <rshanson711@gmail.com>
2023-08-06 20:27:42 -0500

    Initialized integration of Solid.js frontend. Will be replacing Thymeleaf templates in time

---
+++ src/main/java/com/rshanson711/mlbstandingstracker/domain/ApiResponse.java
@@ -0,0 +1,9 @@
+package com.rshanson711.mlbstandingstracker.domain;
+
+public class ApiResponse {
+    public Object body;
+
+    public ApiResponse(Object body) {
+        this.body = body;
+    }
+}
--- src/main/java/com/rshanson711/mlbstandingstracker/web/WebController.java
+++ src/main/java/com/rshanson711/mlbstandingstracker/web/WebController.java
@@ -1,25 +1,44 @@
 package com.rshanson711.mlbstandingstracker.web;
 
+import com.rshanson711.mlbstandingstracker.domain.ApiResponse;
 import com.rshanson711.mlbstandingstracker.domain.DivisionEnum;
 import com.rshanson711.mlbstandingstracker.domain.DivisionStanding;
 import com.rshanson711.mlbstandingstracker.service.MlbApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping(value = "/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class WebController {
     @Autowired
     private MlbApiService mlbApiService;
 
+//    @GetMapping
+//    public List<List<DivisionStanding>> home(@RequestParam(value = "leagueId", defaultValue = "104") int leagueId, @RequestParam(value = "divisionId", defaultValue = "205") int divisionId, Model model) {
+//        String[] leagueLeaders = mlbApiService.getLeagueLeaders();
+//        List<DivisionStanding> alDivisionStandings = new ArrayList<>();
+//        List<DivisionStanding> nlDivisionStandings = new ArrayList<>();
+//        alDivisionStandings.add(mlbApiService.getDivisionalStandings(103, DivisionEnum.AL_EAST));
+//        alDivisionStandings.add(mlbApiService.getDivisionalStandings(103, DivisionEnum.AL_CENTRAL));
+//        alDivisionStandings.add(mlbApiService.getDivisionalStandings(103, DivisionEnum.AL_WEST));
+//
+//        nlDivisionStandings.add(mlbApiService.getDivisionalStandings(104, DivisionEnum.NL_EAST));
+//        nlDivisionStandings.add(mlbApiService.getDivisionalStandings(104, DivisionEnum.NL_CENTRAL));
+//        nlDivisionStandings.add(mlbApiService.getDivisionalStandings(104, DivisionEnum.NL_WEST));
+//        model.addAttribute("divisionStandings", Arrays.asList(alDivisionStandings, nlDivisionStandings));
+//        model.addAttribute("alLeader", leagueLeaders[0]);
+//        model.addAttribute("nlLeader", leagueLeaders[1]);
+//        return Arrays.asList(alDivisionStandings, nlDivisionStandings);
+//    }
+
     @GetMapping("/")
-    public String home(@RequestParam(value = "leagueId", defaultValue = "104") int leagueId, @RequestParam(value = "divisionId", defaultValue = "205") int divisionId, Model model) {
+    public ApiResponse home(@RequestParam(value = "leagueId", defaultValue = "104") int leagueId, @RequestParam(value = "divisionId", defaultValue = "205") int divisionId, Model model) {
         String[] leagueLeaders = mlbApiService.getLeagueLeaders();
         List<DivisionStanding> alDivisionStandings = new ArrayList<>();
         List<DivisionStanding> nlDivisionStandings = new ArrayList<>();
@@ -33,7 +52,7 @@ public class WebController {
         model.addAttribute("divisionStandings", Arrays.asList(alDivisionStandings, nlDivisionStandings));
         model.addAttribute("alLeader", leagueLeaders[0]);
         model.addAttribute("nlLeader", leagueLeaders[1]);
-        return "home";
+        return new ApiResponse(alDivisionStandings.get(0).getName());
     }
 
     @GetMapping("/standings/nlcentral")
